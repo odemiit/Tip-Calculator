a6dac1d5f8cfc8278fd39a21a8fe6ff507d7e99f
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-06-09 19:57:55 +0000

    removing the dollar sign from the total bill and converting the inputs into integer/float

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ print("Welcome to the tip calculator!")
 total_bill = input("What was the total bill? ")
 tip = input("How much tip would you like to give? 10, 12, or 15? ")
 no_of_people = input("How many people to split the bill? ")
+
+#total_bill string to just a number (remove $ sign)
+total_bill = total_bill[1:]
+
+#convert inputs to int or float
+total_bill_as_float = float(total_bill)
+tip_as_int = int(tip)
+no_of_people_as_int = int(no_of_people)
+
+#individual_bill = (total_bill / no_of_people) * (1 + tip / 100)
